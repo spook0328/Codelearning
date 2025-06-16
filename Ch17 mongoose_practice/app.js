e747b86cd8d2fb0c0464c5fef92b15aa85e6aa1d
Eric <eric032807@gmail.com>
2025-06-16 19:01:18 -0400

    mongoos 1

--- Ch17 mongoose_practice/app.js
+++ Ch17 mongoose_practice/app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
 app.set("veiw engine", "ejs");
 
@@ -16,6 +17,36 @@ mongoose
   });
 //若不知道是在哪個地方可以開CMD看，然後打mongosh 可以知道127.0.1後面是什麼
 //再來打show dbs 可以看到要連哪一個db
+
+//製作一個Mongooss schema
+const studentSchema = new Schema({
+  name: String,
+  age: Number,
+  major: String,
+  schoarship: {
+    merit: Number,
+    other: Number,
+  },
+});
+
+//model
+const Student = mongoose.model("Student", studentSchema);
+//製作新object
+const newObject = new Student({
+  name: "Eric",
+  age: 27,
+  major: "Math",
+  Scohlarship: { merit: 6000, other: 7000 },
+});
+newObject
+  .save()
+  .then((saveObject) => {
+    console.log("資料已經儲存完畢，存的資料是:");
+    console.log(saveObject);
+  })
+  .catch((e) => {
+    console.log(e);
+  });
 
 app.listen(3000, () => {
   console.log("伺服器正在聆聽Port 3000...");

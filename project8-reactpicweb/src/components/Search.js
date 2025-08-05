ccd5d6d5d1e66bd5f8a4b7a05b059a1526a7310a
Eric <eric032807@gmail.com>
2025-08-04 23:13:10 -0400

    project 8 finish

--- project8-reactpicweb/src/components/Search.js
+++ project8-reactpicweb/src/components/Search.js
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 
-const Search = ({ search }) => {
+const Search = ({ search, setInput }) => {
+  const inputHandler = (e) => {
+    setInput(e.target.value);
+  };
+
   return (
     <div className="search">
-      <input className="input" type="text" />
+      <input className="input" onChange={inputHandler} type="text" />
       <button onClick={search}>Search</button>
     </div>
   );
--- project8-reactpicweb/src/pages/Homepage.js
+++ project8-reactpicweb/src/pages/Homepage.js
@@ -5,25 +5,59 @@ import Picture from "../components/Picture";
 // logical operator &&
 
 const Homepage = () => {
+  let [input, setInput] = useState("");
   let [data, setData] = useState(null);
+  let [page, setPage] = useState(1);
+  let [currentSearch, setCurrentSearch] = useState("");
   const auth = "Xenb6GvuH4VD3FoM9A9a3IX8M9POQL1a5WUculIKDTTaq8f5eirvL9KA";
   const initialURL = "https://api.pexels.com/v1/curated?page=1&per_page=15";
+  let searchURL = `https://api.pexels.com/v1/search?query=${input}$per_page=15&page=1`;
 
-  const search = async () => {
-    let result = await axios.get(initialURL, {
+  const search = async (url) => {
+    let result = await axios.get(url, {
       headers: { Authorization: auth },
     });
     setData(result.data.photos);
+    setCurrentSearch(input);
   };
+
+  //Closure
+  const morePicture = async () => {
+    let newURL;
+    setPage(page + 1);
+    if (currentSearch == "") {
+      newURL = `https://api.pexels.com/v1/curated?page=${page + 1}&per_page=15`;
+    } else {
+      newURL = `https://api.pexels.com/v1/search?query=${currentSearch}$per_page=15&page=${
+        page + 1
+      }`;
+    }
+    let result = await axios.get(newURL, {
+      headers: { Authorization: auth },
+    });
+    setData(data.concat(result.data.photos));
+  };
+
+  useEffect(() => {
+    search(initialURL);
+  }, []);
 
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Search search={search} />
+      <Search
+        search={() => {
+          search(searchURL);
+        }}
+        setInput={setInput}
+      />
       <div className="pictures">
         {data &&
           data.map((d) => {
             return <Picture data={d} />;
           })}
+      </div>
+      <div className="morePicture">
+        <button onClick={morePicture}>更多圖片</button>
       </div>
     </div>
   );

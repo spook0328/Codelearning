44493269bd8353d691c0bae5e663fe22293d0bc1
Eric <eric032807@gmail.com>
2025-08-18 14:48:21 -0400

    router add delete edit

--- project9 MERN-TeachingWebsite/routes/course-route.js
+++ project9 MERN-TeachingWebsite/routes/course-route.js
@@ -23,7 +23,7 @@ router.get("/", async (req, res) => {
 });
 
 //用課程id尋找課程
-router.get(":_id", async (req, res) => {
+router.get("/:_id", async (req, res) => {
   let { _id } = req.params;
   try {
     let courseFound = await Course.findOne({ _id })
@@ -59,6 +59,61 @@ router.post("/", async (req, res) => {
     return res.send("新課程已保存");
   } catch (e) {
     return res.status(500).send("無法創建課程...");
+  }
+});
+
+//更改課程
+router.patch("/:_id", async (req, res) => {
+  //驗證數據符合規範
+  let { error } = courseValidation(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  let { _id } = req.params;
+
+  //確認課程是否存在
+  try {
+    let courseFound = await Course.findOne({ _id });
+    if (!courseFound) {
+      return res.status(400).send("找不到課程。無法更新課程。");
+    }
+
+    //使用者必須是此課程講師
+    if (courseFound.instructor.equals(req.user._id)) {
+      let updatedCourse = await Course.findOneAndUpdate({ _id }, req.body, {
+        new: true,
+        runValidators: true,
+      });
+      return res.send({
+        message: "課程已經被更新成功",
+        updatedCourse,
+      });
+    } else {
+      return res.status(403).send("只有該堂課講師可以編輯課程");
+    }
+  } catch (e) {
+    return res.status(500).send(e);
+  }
+});
+
+//刪除課程
+router.delete("/:_id", async (req, res) => {
+  let { _id } = req.params;
+  //確認課程是否存在
+  try {
+    let courseFound = await Course.findOne({ _id }).exec();
+    if (!courseFound) {
+      return res.status(400).send("找不到課程。無法刪除課程。");
+    }
+
+    //使用者必須是此課程講師，才能刪除課程
+    if (courseFound.instructor.equals(req.user._id)) {
+      await Course.deleteOne({ _id }).exec();
+      return res.send("成功刪除課程");
+    } else {
+      return res.status(403).send("只有該堂課講師可以刪除課程");
+    }
+  } catch (e) {
+    return res.status(500).send(e);
   }
 });
 

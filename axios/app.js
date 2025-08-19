c7949c53040782295789338e30e4fcb615123dab
Eric <eric032807@gmail.com>
2025-08-19 00:42:32 -0400

    介紹axios

--- axios/app.js
+++ axios/app.js
@@ -1,4 +1,6 @@
 //介紹前端套件 axios 使用
+//用fetch 方式，他會return 一個promise object
+//但這裡面promise object 是一個responseObject
 async function example1() {
   try {
     let responseObject = await fetch(
@@ -11,4 +13,18 @@ async function example1() {
   }
 }
 
+//用axios 方式，也會return 一個promise object
+//但這裡面promise object 是一個獨特的axios Response object
+async function example2(params) {
+  try {
+    let axiosResponseObject = await axios.get(
+      "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+    );
+    console.log(axiosResponseObject.data);
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 example1();
+example2();
